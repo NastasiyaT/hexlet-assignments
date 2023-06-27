64f8d5f146e58c32b5a53cd230d05ffabf278a01
@hexlet/cli <support@hexlet.io>
2023-06-27 22:13:24 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -4,34 +4,20 @@ import java.util.Arrays;
 
 // BEGIN
 public class App {
-    public static String[][] enlargeArrayImage(String[][] images) {
-        var row = images[0].length * 2;
-        var column = images.length * 2;
-        String[][] newRows = new String[column / 2][row];
+    public static String[] duplicateValues(String[] items) {
+        return Arrays.stream(items)
+                .flatMap(item -> Arrays.stream(new String[] {item, item}))
+                .toArray(String[]::new);
+    }
 
-        for (String[] i : images) {
-            for (String[] nI : newRows) {
-                Arrays.stream(i)
-                        .forEach(t -> {
-                            int m = 0;
-                            nI[m] = t;
-                            nI[m + 1] = t;
-                            m += 2;
-                        });
-            }
-        }
+    public static String[][] enlargeArrayImage(String[][] chars) {
+        String[][] doubleRows = Arrays.stream(chars)
+                .map(App::duplicateValues)
+                .toArray(String[][]::new);
 
-        String[][] newImages = new String[column][row];
-
-        Arrays.stream(newRows)
-                .forEach(e -> {
-                    int p = 0;
-                    newImages[p] = e;
-                    newImages[p + 1] = e;
-                    p += 2;
-                });
-
-        return newImages;
+        return Arrays.stream(doubleRows)
+                .flatMap(issue -> Arrays.stream(new String[][] {issue, issue}))
+                .toArray(String[][]::new);
     }
 }
 // END
